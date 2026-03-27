8ec01762dfff513da25929f52c1e6f00a7288319
iammadhu123 <myadav07012006@gmail.com>
2026-03-27 21:28:35 +0530

    committed

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const path = require('path'); // ejs files are in views folder
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const MONGO_URL = 'mongodb://localhost:27017/wanderlust';
 
@@ -23,6 +24,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Set the views directory
 app.use(express.urlencoded({extended: true})); // To parse form data
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.get('/', (req, res) => {
     res.send('Hi, I am root');
